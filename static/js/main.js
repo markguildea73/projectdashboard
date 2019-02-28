b8a69ef7e9e55f4c3365f9ebef5cc4b1a36b7a56
Mark Guildea <mark.anthony.guildea@gmail.com>
2019-02-28 21:33:02 +0000

    adding some graphs

---
+++ static/js/main.js
@@ -0,0 +1,199 @@
+queue()
+        .defer(d3.csv, "/data/amb.csv")
+        .await(makeGraphs);
+        
+    function makeGraphs(error, data) {
+        var ndx = crossfilter(data);
+        
+        // date parsing
+        
+        var parseDate = d3.time.format("%d/%m/%Y %H:%M").parse;
+        data.forEach(function(d){
+            d.date = parseDate(d.date);
+            d.temp1 = +d.temp1;
+            d.temp2 = +d.temp2;
+            d.temp3 = +d.temp3;
+            d.temp4 = +d.temp4;
+            
+        });
+        
+        // charts 
+        
+        
+        show_composite_trend(ndx);
+        
+        show_scatter_plot(ndx);
+        
+        show_scatter_plot_2(ndx);
+        
+        data_list(ndx);
+        
+        
+        // render charts
+        dc.renderAll();
+        
+        
+        
+    }
+    function show_composite_trend(ndx){
+        var date_dim = ndx.dimension(dc.pluck('date'));
+        var minDate = date_dim.bottom(1)[0].date;
+        var maxDate = date_dim.top(1)[0].date;
+        
+        
+        var temp1Data = date_dim.group().reduceSum(dc.pluck("temp1"));
+        var temp2Data = date_dim.group().reduceSum(dc.pluck("temp2"));
+        var temp3Data = date_dim.group().reduceSum(dc.pluck("temp3"));
+        var temp4Data = date_dim.group().reduceSum(dc.pluck("temp4"));
+        
+        var compositeChart = dc.compositeChart('.coldroom_trend');
+        
+        compositeChart
+            .width(600)
+            .height(130)
+            .dimension(date_dim)
+            .x(d3.time.scale().domain([minDate, maxDate]))
+            .y(d3.scale.linear().domain([0,30]))
+            .yAxisLabel("Temperature")
+            .xAxisLabel("Date")
+            .legend(dc.legend().x(80).y(20).itemHeight(13).gap(5))
+            .renderHorizontalGridLines(true)
+            .elasticX(false)
+            .yAxisPadding(5)
+            .compose([
+                dc.lineChart(compositeChart)
+                    .colors('green')
+                    .group(temp1Data, "temp1"),
+                dc.lineChart(compositeChart)
+                    .colors('red')
+                    .group(temp2Data, "temp2"),
+                dc.lineChart(compositeChart)
+                    .colors('blue')
+                    .group(temp3Data, "temp3"),
+                dc.lineChart(compositeChart)
+                    .colors('orange')
+                    .group(temp4Data, "temp4"),
+            ])
+            .brushOn(false)
+            // .render()
+        }
+        
+        function show_scatter_plot(ndx){
+        var date_dim = ndx.dimension(dc.pluck('date'));
+        var minDate = date_dim.bottom(1)[0].date;
+        var maxDate = date_dim.top(1)[0].date;
+       
+        
+        var temp_dim = ndx.dimension(function (d) {
+            return [d.date, d.temp1];
+            })
+        var temp_group = temp_dim.group();
+        var temp_chart = dc.scatterPlot(".ambient_trend");
+            
+            temp_chart
+                .width(600)
+                .height(130)
+                .x(d3.time.scale().domain([minDate, maxDate]))
+                .yAxisLabel("Temperature")
+                .xAxisLabel("Date")
+                .legend(dc.legend().x(80).y(20).itemHeight(13).gap(5))
+                .y(d3.scale.linear().domain([0,30]))
+                .renderHorizontalGridLines(true)
+                .brushOn(false)
+                .symbolSize(2)
+                .clipPadding(10)
+                .dimension(temp_dim)
+                .group(temp_group, "Temp3")
+
+        }
+        
+        function show_scatter_plot_2(ndx){
+        var date_dim = ndx.dimension(dc.pluck('date'));
+        var minDate = date_dim.bottom(1)[0].date;
+        var maxDate = date_dim.top(1)[0].date;
+      
+    
+        
+        
+        var temp_dim_1 = ndx.dimension(function (d) {
+        return [d.date, d.temp1];
+        })
+        var temp_dim_2 = ndx.dimension(function (d) {
+        return [d.date, d.temp2];
+        })
+        var temp_dim_3 = ndx.dimension(function (d) {
+        return [d.date, d.temp3];
+        })
+        var temp_group_1 = temp_dim_1.group();
+        var temp_group_2 = temp_dim_2.group();
+        var temp_group_3 = temp_dim_3.group();
+        
+        var scatter = dc.compositeChart('.coldroom_table');
+        
+        scatter
+            .width(600)
+            .height(130)
+            .dimension(date_dim)
+            .x(d3.time.scale().domain([minDate, maxDate]))
+            .y(d3.scale.linear().domain([0,30]))
+            .yAxisLabel("Temperature")
+            .xAxisLabel("Date")
+            .legend(dc.legend().x(80).y(2).itemHeight(13).gap(5))
+            .renderHorizontalGridLines(true)
+            .elasticX(false)
+            .compose([
+                dc.scatterPlot(scatter)
+                    .colors('green')
+                    .group(temp_group_1, "temp1")
+                    .symbolSize(2)
+                    .clipPadding(10),
+                dc.scatterPlot(scatter)
+                    .colors('red')
+                    .group(temp_group_2, "temp2")
+                    .symbolSize(2)
+                    .clipPadding(10),
+                dc.scatterPlot(scatter)
+                    .colors('blue')
+                    .group(temp_group_3, "temp3")
+                    .symbolSize(2)
+                    .clipPadding(10)
+            ])
+            .brushOn(false)
+            
+         }
+        
+        function data_list(ndx){
+            d3.csv("data/amb.csv", function(error, data) {
+		  if (error) throw error;
+		  
+		  console.log(data)
+		  
+		  var sortAscending = true;
+		  var table = d3.select('.ambient_table').append('table');
+		  var titles = d3.keys(data[0]);
+		  var headers = table.append('thead').append('tr')
+		                   .selectAll('th')
+		                   .data(titles).enter()
+		                   .append('th')
+		                   .text(function (d) {
+			                    return d;
+		                    });
+		  
+		  var rows = table.append('tbody').selectAll('tr')
+		               .data(data).enter()
+		               .append('tr');
+		  rows.selectAll('td')
+		    .data(function (d) {
+		    	return titles.map(function (k) {
+		    		return { 'value': d[k], 'name': k};
+		    	});
+		    }).enter()
+		    .append('td')
+		    .attr('data-th', function (d) {
+		    	return d.name;
+		    })
+		    .text(function (d) {
+		    	return d.value;
+		    });
+	  });
+        }
